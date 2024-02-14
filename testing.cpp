6f1060270dd93b6858563947ed584e9af353ce32
Bhupen <bhupenoffline@gmail.com>
2024-02-14 23:51:18 +0000

    test commit

--- testing.cpp
+++ testing.cpp
@@ -4,11 +4,16 @@ float testFunction(float num1, float num2){
     float total = num1 + num2;
     return total;
 }
+int returnInteger(int num){
+    int result = num * num;
+    return result;
+}
 int main(){
     std::cout << "hi";
     std::cout << "hello";
     std::cout << "how are you doing";
-    kdsfohipoajfepoiaji
 
-    std::cout << "\n" << testFunction(4.5, 8.32);
+    std::cout << "\n" << testFunction(4.5, 8.32) << "\n";
+
+    std::cout << returnInteger(5);
 }
